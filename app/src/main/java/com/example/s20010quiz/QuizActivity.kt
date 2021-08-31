3b154f5fd85f194624eba7f8cb6d4ee130b53c14
= <=>
2021-08-31 15:24:23 +0900

    もう少し

--- app/src/main/java/com/example/s20010quiz/QuizActivity.kt
+++ app/src/main/java/com/example/s20010quiz/QuizActivity.kt
@@ -2,26 +2,32 @@ package com.example.s20010quiz
 
 import android.app.AlertDialog
 import android.app.AppComponentFactory
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.RadioButton
 import androidx.appcompat.app.AppCompatActivity
 import com.example.s20010quiz.databinding.ActivityQuizBinding
+import android.os.CountDownTimer
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import kotlin.system.measureTimeMillis
 
 class QuizActivity : AppCompatActivity() {
     private lateinit var binding: ActivityQuizBinding
-
-    private lateinit var parent_list: ArrayList<List<String>>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityQuizBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+        val start = System.currentTimeMillis();
+        println(start)
 
 
         //自身のインテントを受け取る
         var index = intent.getIntExtra("INDEX", 1)
         var score = intent.getIntExtra("SCORE", 0)
+        var time = intent.getLongExtra("TIME",0)
 
         //タイマーを作る
 
@@ -62,8 +68,6 @@ class QuizActivity : AppCompatActivity() {
         //２次元リストをつくる
         val parent_list = arrayListOf(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11)
 
-        binding.Question1.text = parent_list[1][0]
-
 
         //４択をランダムに表示するためのリストをつくる
         val list = listOf(2, 3, 4, 5)
@@ -74,7 +78,7 @@ class QuizActivity : AppCompatActivity() {
 
         //答えを保持
         val answer = q[2]
-
+        //ビューに値を代入
         binding.SubTitle.text = "第 ${index.toString()} 問"
         binding.Question1.text = q[0]
         binding.select1.text = q[num[0]]
@@ -82,20 +86,87 @@ class QuizActivity : AppCompatActivity() {
         binding.select3.text = q[num[2]]
         binding.select4.text = q[num[3]]
 
+        //タイマーのインスタンス生成
+        val timer = object : CountDownTimer(
+            10000,
+            1000
+        ) {
+            override fun onTick(millisUntilFinished: Long) {
+                val second = millisUntilFinished / 1000L % 60L
+                binding.Timer.text = second.toString()
+            }
+            //終了したら、NEXTボタンをクリック
+            override fun onFinish() {
+                binding.Next.performClick()
+            }
+        }.start()
+        //10問目まで繰り返す
         index++
 
-        val id = binding.Selecters.checkedRadioButtonId
-        var takeanswer = findViewById<RadioButton>(id)
-        val decision = takeanswer.text
+        val times = measureTimeMillis {
+            timer.start()
+        }
+        println(times)
 
-        if (decision == answer) {
+        val handler = Handler(Looper.getMainLooper())
+        binding.Next.setOnClickListener {
+
+        //選ばれているボタンのテキストを取得
+        val id = binding.Selecters.checkedRadioButtonId
+        val radioButton = findViewById<RadioButton>(id)
+        val selectText = radioButton.text
+
+        //正解の場合、
+        if(selectText == answer) {
             binding.Question1.text = "正解"
             score++
-        } else {
+        }
+
+        //不正解の場合
+        else
             binding.Question1.text = "不正解"
+
+
+    }
+        //次の画面へ遷移
+        binding.Next.setOnClickListener {
+            //10を超えた場合は最終結果へ
+            if (index > 10) {
+                timer.cancel()
+                val end = System.currentTimeMillis()
+                //時間の計算
+                time += (end - start) / 1000
+                onChange(it,index, score, times)
+            }
+            //10以下の場合は次の問題へ
+            else {
+                timer.cancel()
+                val end = System.currentTimeMillis()
+                //時間の計算
+                time += (end - start) / 1000
+                onChange(it, index, score,time)
+            }
         }
 
     }
+    //indexが１０以下の処理
+    fun onChange (view: View?, index:Int, score: Int, time: Long) {
+        val intent = Intent(this, QuizActivity::class.java)
+        intent.putExtra("INDEX", index)
+        intent.putExtra("SCORE", score)
+        intent.putExtra("TIME",time)
+        startActivity(intent)
+    }
+    //indexが１０を超えた時最終画面へ遷移
+    fun resultChange (view: View?, score: Int, time: Long) {
+
+        val intent = Intent(this, ResultActivity::class.java)
+        intent.putExtra("RESULT_SCORE", score.toString())
+        intent.putExtra("RESULT_TIME", time.toString())
+        startActivity(intent)
+    }
+
+
 }
 
 
---
+++ app/src/main/java/com/example/s20010quiz/Result.kt
@@ -0,0 +1,27 @@
+package com.example.s20010quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.s20010quiz.databinding.ActivityResultBinding
+import android.content.Intent
+import android.view.View
+import com.example.s20010quiz.databinding.ActivityQuizBinding
+import java.util.zip.Inflater
+
+class ResultActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityResultBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //最終結果を受け取る処理
+        val resultScore = intent.getStringExtra("RESULT_SCORE")
+        val resultTime = intent.getStringExtra("RESULT_TIME")
+
+        //正解数を表示
+        binding.resultScore.text = resultScore
+        binding.resultTime.text = resultTime
+    }
+}
