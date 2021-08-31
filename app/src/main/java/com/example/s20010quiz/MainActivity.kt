33b73d4deed79dba4f40de9b52946f393ea4a380
= <=>
2021-08-31 10:32:42 +0900

    もう少し

--- app/src/main/java/com/example/s20010quiz/MainActivity.kt
+++ app/src/main/java/com/example/s20010quiz/MainActivity.kt
@@ -16,79 +16,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.Try.setOnClickListener { onChange(it) }
-        }
-    fun onChange (view: View?) {
+    }
+
+    fun onChange(view: View?) {
         val intent = Intent(this, QuizActivity::class.java)
         startActivity(intent)
 
-        }
-    }
-
-
-/*
-class MainActivity : AppCompatActivity() {
-    // タイトルの配列と、選択肢の２次配列　（正解、選択肢１、選択肢２，選択肢３の順番）
-    private val quizTitle = arrayOf("１問目","２問目","３問目","４問目")
-    private val quizData = arrayOf(
-        arrayOf("A0","A1","A2","A3"),
-        arrayOf("B0","B1","B2","B3"),
-        arrayOf("C0","C1","C2","C3"),
-        arrayOf("D0","D1","D2","D3")
-    )
-    //カウントの変数
-    private var i = 0
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        //viewを取得
-        val question1 : TextView = findViewById(R.id.Question1)
-        val btn0 : Button = findViewById(R.id.Btn0)
-        val btn1 : Button = findViewById(R.id.Btn1)
-        val btn2 : Button = findViewById(R.id.Btn2)
-        val btn3 : Button = findViewById(R.id.Btn3)
-        val next : Button = findViewById(R.id.Next)
-
-        //カウント数と、最初の問題を表示
-        question1.text = quizTitle[0]
-
-        //0-3までのリスト用意=>シャッフル
-        val list = listOf(0,1,2,3)
-        val num = list.shuffled()
-
-        //ボタンにquizDataを表示して、Nextボタンは無効化
-        //シャッフルしたnumを表示
-
-        btn0.text = quizData[0][num[0]]
-        btn1.text = quizData[0][num[1]]
-        btn2.text = quizData[0][num[2]]
-        btn3.text = quizData[0][num[3]]
-        next.isEnabled = false
-
-
-
-        //btn0を押したときの正誤判定
-        btn0.setOnClickListener {
-            if (btn0.text == quizData[i][0]) {
-                //正解アラートダイアログ
-                AlertDialog.Builder(this)
-                    .setTitle("正解！")
-                    .setMessage(quizData[i][0])
-                    .setPositiveButton("OK",null)
-                    .show()
-                //正解したら回答ボタンを無効化とNEXTボタンを有効化
-                btn0.isEnabled = false
-                btn1.isEnabled = false
-                btn2.isEnabled = false
-                btn3.isEnabled = false
-                next.isEnabled = true
-            } else {
-                //不正解
-                question1.text = "不正解"
-            }
-        }
     }
 }
-
- */
--- app/src/main/java/com/example/s20010quiz/QuizActivity.kt
+++ app/src/main/java/com/example/s20010quiz/QuizActivity.kt
@@ -1,14 +1,17 @@
 package com.example.s20010quiz
 
+import android.app.AlertDialog
 import android.app.AppComponentFactory
 import android.os.Bundle
+import android.widget.Button
+import android.widget.RadioButton
 import androidx.appcompat.app.AppCompatActivity
 import com.example.s20010quiz.databinding.ActivityQuizBinding
 
 class QuizActivity : AppCompatActivity() {
     private lateinit var binding: ActivityQuizBinding
 
-    private lateinit var parent_list : ArrayList<List<String>>
+    private lateinit var parent_list: ArrayList<List<String>>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityQuizBinding.inflate(layoutInflater)
@@ -17,13 +20,13 @@ class QuizActivity : AppCompatActivity() {
 
 
         //自身のインテントを受け取る
-        var index = intent.getIntExtra("INDEX",1)
-        var score = intent.getIntExtra("SCORE",0)
+        var index = intent.getIntExtra("INDEX", 1)
+        var score = intent.getIntExtra("SCORE", 0)
 
         //タイマーを作る
 
         //CSVを格納
-        val assetManager = applicationContext.assets.open( "s20010_quiz.csv").bufferedReader()
+        val assetManager = applicationContext.assets.open("s20010_quiz.csv").bufferedReader()
         var a = assetManager.use() {
             it.readText()
         }
@@ -55,7 +58,7 @@ class QuizActivity : AppCompatActivity() {
         val a11 = b.take(6)
         b = b.drop(6)
 
-        //val array = Array(6) { IntArray(10) }
+
         //２次元リストをつくる
         val parent_list = arrayListOf(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11)
 
@@ -63,7 +66,7 @@ class QuizActivity : AppCompatActivity() {
 
 
         //４択をランダムに表示するためのリストをつくる
-        val list = listOf(2,3,4,5)
+        val list = listOf(2, 3, 4, 5)
         val num = list.shuffled()
 
         //今の問題に対応するリストを２次元リストから変数に代入
@@ -72,14 +75,29 @@ class QuizActivity : AppCompatActivity() {
         //答えを保持
         val answer = q[2]
 
-        binding.Question1.text = parent_list[1][0]
+        binding.SubTitle.text = "第 ${index.toString()} 問"
+        binding.Question1.text = q[0]
+        binding.select1.text = q[num[0]]
+        binding.select2.text = q[num[1]]
+        binding.select3.text = q[num[2]]
+        binding.select4.text = q[num[3]]
 
+        index++
 
+        val id = binding.Selecters.checkedRadioButtonId
+        var takeanswer = findViewById<RadioButton>(id)
+        val decision = takeanswer.text
 
-        //それぞれのビューに値を代入
-
-        //10問目まで数える
-
+        if (decision == answer) {
+            binding.Question1.text = "正解"
+            score++
+        } else {
+            binding.Question1.text = "不正解"
+        }
 
     }
 }
+
+
+
+
